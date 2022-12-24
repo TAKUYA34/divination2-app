a6d938d639486c9f31937cff8b3db5476462d958
TAKUYA34 <110417799+TAKUYA34@users.noreply.github.com>
2022-12-24 18:45:00 +0900

    投稿ページ、詳細機能の実装、各設定

--- app/controllers/augurys_controller.rb
+++ app/controllers/augurys_controller.rb
@@ -4,4 +4,26 @@ class AugurysController < ApplicationController
     @augurys = Augury.all
   end
 
+  def new
+    @augury = Augury.new
+  end
+
+  def create
+    @augury = Augury.new(augury_params)
+    if @augury.save
+      redirect_to augury_path(@augury)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @augury = Augury.find(params[:id])
+  end
+
+    private
+
+  def augury_params
+    params.require(:augury).permit(:nickname, :sign, :birth_id).merge(user_id: current_user.id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   root to: "augurys#index"
-  resources :augurys, only: [:index]
+  resources :augurys, only: [:index, :new, :create, :show] do
+    resources :results, only: [:index]
+  end
 end
