2ba076ea161c7e081eadd7d27d5c01beddb1a79e
TAKUYA34 <110417799+TAKUYA34@users.noreply.github.com>
2022-12-24 18:00:38 +0900

    deviseモデル設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_one :augury
+  has_one :result
+
+  validates :name, presence: true
+  validates :birth_id, presence: true
 end
